e5fe3743fa383946370e84a2aeb92cecdf2849c6
Abdulsamad <almuhandith1497@gmail.com>
2023-05-11 15:34:51 +0100

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 """Function that prints the titles of first 10 posts"""
+import requests
+
 
 def top_ten(subreddit):
     """Returns first 10 posts listed in a given subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json().get("data")
+        for entry in data.get("children"):
+            print(entry.get("data").get("title"))
+    else:
+        print("None")
+        return
